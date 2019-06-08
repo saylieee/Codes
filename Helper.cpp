bf6a16f3b3366791d8ad4ea27a1c5b7e2bf35734
MK <moghilimanikanta98@gmail.com>
2019-06-08 12:07:11 +0530

    Helper added

---
+++ Helper.cpp
@@ -0,0 +1,33 @@
+28/9/2019
+
+int min = *min_element(v.begin(),v.end());
+
+/*
+ min is  assigned with minimum value of vector.
+ Use:- no need to run loop to find min value.
+ similary *max_element(v.begin(),v.end()) for maximum element.
+ */
+----------------------------------------------------------------------
+
+reverse(v.begin(),v.end());
+/*
+ reverses the contents of vector.
+ */
+
+----------------------------------------------------------------------
+
+vector<int> a;
+a.resize(n);
+
+/*
+ If you want to new vector of similar size
+ */
+
+----------------------------------------------------------------------
+
+fill(v.begin(), v.end(), 0);
+/*
+ set all elements in vector to 0.
+ */
+-----------------------------------------------------------------------
+
